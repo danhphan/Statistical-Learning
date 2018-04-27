97f80897b4067380b894dd0d171addc56ded4edc
unknown <danh.phan.mq@gmail.com>
2018-04-27 21:45:16 +1000

    Time series with ts feature.

--- Time.Series.Analysis.R
+++ Time.Series.Analysis.R
@@ -1,2 +1,12 @@
 
 # Time series analysis
+# get data
+$wget–no-check-certificate–progress=dot https://data.cityofchicago.org/api/views/ijzp-q8t2/rows.csv?accessType=DOWNLOAD > chicago_crime_data.csv
+
+library(data.table)
+dat = fread("chicago_crime_data.csv")
+colnames(dat) = gsub(" ", "_", tolower(colnames(dat)))
+dat[, date2 := as.Date(date, format="%m/%d/%Y")]
+mydat = dat[primary_type=="THEFT", .N, by=date2][order(date2)]
+mydat[1:6]
+
