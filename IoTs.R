ec177c43eb7f3eb8da19daa07767ab5aabe901ee
unknown <danh.phan.mq@gmail.com>
2018-04-28 22:47:20 +1000

    Density map in different hours.

--- IoTs.R
+++ IoTs.R
@@ -6,6 +6,8 @@ gc()
 
 # Load library and data sources
 library(readr)
+library(ggplot2)
+
 mdata <- read_csv("D:/Drive/Persona/MIT Study/R Projects/Statistical Learning/data/MPID_data.csv")
 mpostcodes <- read_csv("D:/Drive/Persona/MIT Study/R Projects/Statistical Learning/data/MPID_Postcodes.csv")
 
@@ -37,9 +39,6 @@ head(mdata[,20:28])
 
 #### QUESTION? ####
 ## PEAK HOURS EACH DAY ##
-
-library(ggplot2)
-
 ggplot(data=mdata,aes(x=`Arrival Time`,y=Bus)) +
   geom_point()
 
@@ -51,6 +50,63 @@ boxplot(mdata$`Arrival Time`~mdata$`Travel Mode - Mon`)
 test <- mdata %>% group_by(`Arrival Time`) %>% summarise(CountN =n())
 test
 
+testdata <- mdata %>% left_join(mpostcodes,by=c("Postcode"="Pcode"))
+dim(mdata)
+dim(testdata)
+
 ggplot(data=test,aes(x=`Arrival Time`,y=CountN)) +
   geom_point()
 hist(test$CountN~test$`Arrival Time`)
+
+str(mpostcodes)
+shapefile <- mpostcodes
+data <- fortify(shapefile)
+data
+str(mdata)
+
+################################################################
+# Print Map
+library(stringr)
+testdata$`Arrival Time`
+test2 <- as.character(testdata$`Arrival Time`)
+test2 <- str_sub(test2,1,2)
+test2
+testdata$hours <- test2
+
+loc <- geocode("21 Byfield St, Macquarie Park NSW 2113")
+loc <- as.numeric(loc)
+loc
+macqpark <-get_map(location=loc,zoom=11)
+macqmap <- ggmap(macqpark, extent = "device", legend = "topleft")
+
+# Check from 5am, 6am, 7am
+sub_data <- testdata[which(testdata$hours %in% c("05","06","07")),]
+macqmap +
+  stat_density2d(
+    aes(x = Long, y = Lat, fill = ..level.., alpha = ..level..),
+    size = 2, bins = 4, data = sub_data,
+    geom = "polygon"
+  ) +
+  scale_fill_gradient(low = "black", high = "red") + 
+  facet_wrap(~hours)
+
+
+#############################
+# Test qmap
+
+help(qmap)
+
+qmap('2 Lyonpark Rd Macquarie Park NSW 2113', zoom = 15, maptype = 'hybrid') +
+  geom_polygon(aes(x = Long, y = Lat, group = Area), data = data,
+               colour = 'white', fill = 'black', alpha = .4, size = .3)
+
+# qmap(location = "Macquarie university")
+# qmap(location = "Macquarie university", zoom = 14)
+# qmap(location = "20 ethel street, eastwood, nsw, 2122", zoom = 14, source = "osm")
+# qmap(location = "baylor university", zoom = 14, source = "osm", scale = 20000)
+# qmap(location = "baylor university", zoom = 14, maptype = "satellite")
+# qmap(location = "baylor university", zoom = 14, maptype = "hybrid")
+# qmap(location = "baylor university", zoom = 14, maptype = "toner", source = "stamen")
+# qmap(location = "baylor university", zoom = 14, maptype = "watercolor", source = "stamen")
+# qmap(location = "Macquarie university", zoom = 14, maptype = "terrain-background", source = "stamen")
+# qmap(location = "baylor university", zoom = 14, maptype = "toner-lite", source = "stamen")
